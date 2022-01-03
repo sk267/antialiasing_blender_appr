f7fd76ecdfeb2d4d0da4062d611c525d0c62cf66
Steffen <steffen1.koehler@web.de>
2022-01-03 10:39:51 +0100

    rgb works

--- addBlackBetween.py
+++ addBlackBetween.py
@@ -5,22 +5,24 @@ import numpy
 
 IN_SIZE_X = 1920
 IN_SIZE_Y = 1080
-N_BLACK_PIXELS = 5
+N_BLACK_PIXELS = 3
 
 OUT_SIZE_X = IN_SIZE_X * N_BLACK_PIXELS + 1
 OUT_SIZE_Y = IN_SIZE_Y * N_BLACK_PIXELS + 1
 
 
-result = numpy.full((OUT_SIZE_Y, OUT_SIZE_X), 0)
+result = numpy.full((OUT_SIZE_Y, OUT_SIZE_X, 3), 0)
 
 
-img = cv2.imread("example.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("./../example.jpg")
 
-print(img.shape)
+# plt.imshow(img)
+# plt.show()
+
+# print(img.shape)
 
 counter_x = 0
 counter_y = 0
-
 
 
 for y in range(OUT_SIZE_Y):
@@ -34,15 +36,15 @@ for y in range(OUT_SIZE_Y):
 
         if x == 0:
             continue
-
+        
         if x % N_BLACK_PIXELS == 0:
-            result[y][x] = img[counter_y][counter_x]
+            for color in range(3):
+                result[y][x][color] = img[counter_y][counter_x][color]
             counter_x += 1
 
     counter_x = 0
     counter_y += 1
 
 
-cv2.imwrite("result.png", result)
-# plt.imshow(result, cmap="gray")
-# plt.show()
+cv2.imwrite("./../result.png", result)
+
